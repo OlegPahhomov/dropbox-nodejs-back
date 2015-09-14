9e7ee5909ecb2158f2842ba3c438df58f0838a47
oleg.pahhomov <oleg.pahhomov@gmail.com>
2015-09-14 14:54:36 +0300

    added getImage and getThumbnail, refactored to 2 files

--- routes/getfiles.js
+++ routes/getfiles.js
@@ -1,29 +1,57 @@
-router.get('/files', function(req, res) {
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://postgres:12345@localhost:5432/postgres';
 
-    var results = [];
-
-    // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) {
-
-        // SQL Query > Select Data
-        var query = client.query("SELECT * FROM FILEitems ORDER BY id ASC;");
-
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
+module.exports = {
+    getFiles: function (req, res) {
+        var results = [];
+        pg.connect(connectionString, function (err, client, done) {
+            var query = client.query("SELECT * FROM FILE");
+            query.on('row', function (row) {
+                results.push(row);
+            });
+            query.on('end', function () {
+                client.end();
+                return res.json(results);
+            });
+            if (err) {
+                console.log(err);
+            }
         });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            client.end();
-            return res.json(results);
+    },
+    getPicture: function (req, res){
+        var results = [];
+        var id = req.params.id;
+        pg.connect(connectionString, function (err, client, done) {
+            var query = client.query("select content from content where file_id = ($1) and type = 'picture'", [id]);
+            query.on('row', function (row) {
+                results.push(row.content);
+            });
+            query.on('end', function () {
+                client.end();
+                res.end(results[0], 'binary');
+                return res;
+            });
+            if (err) {
+                console.log(err);
+            }
         });
-
-        // Handle Errors
-        if(err) {
-            console.log(err);
-        }
-
-    });
-
-});
+    },
+    getThumbnail: function (req, res){
+        var results = [];
+        var id = req.params.id;
+        pg.connect(connectionString, function (err, client, done) {
+            var query = client.query("select content from content where file_id = ($1) and type = 'thumbnail'", [id]);
+            query.on('row', function (row) {
+                results.push(row.content);
+            });
+            query.on('end', function () {
+                client.end();
+                res.end(results[0], 'binary');
+                return res;
+            });
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,21 @@
 var express = require('express');
 var router = express.Router();
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://postgres:12345@localhost:5432/postgres';
+
+var dbOperations = require('./getfiles.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-
-router.get('/files', function(req, res) {
-
-  var results = [];
-
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, function(err, client, done) {
-
-    // SQL Query > Select Data
-    var query = client.query("SELECT * FROM FILE;");
-
-    // Stream results back one row at a time
-    query.on('row', function(row) {
-      results.push(row);
-    });
-
-    // After all data is returned, close connection and return results
-    query.on('end', function() {
-      client.end();
-      return res.json(results);
-    });
-
-    // Handle Errors
-    if(err) {
-      console.log(err);
-    }
-
-  });
-
+router.get('/files', function (req, res) {
+    dbOperations.getFiles(req, res);
+});
+router.get('/picture/:id', function (req, res) {
+    dbOperations.getPicture(req, res);
+});
+router.get('/picture/small/:id', function (req, res) {
+    dbOperations.getThumbnail(req, res);
 });
 
 
