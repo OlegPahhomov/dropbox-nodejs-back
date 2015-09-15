4c7c0dab1a463b3ff0952013a8b218f02023e630
oleg.pahhomov <oleg.pahhomov@gmail.com>
2015-09-15 20:57:13 +0300

    made delete nicer, bug on addFiles

--- routes/postFiles.js
+++ routes/postFiles.js
@@ -1,5 +1,4 @@
 var pg = require('pg');
-var util = require('util');
 var formidable = require('formidable');
 var connectionString = process.env.DATABASE_URL || 'postgres://postgres:12345@localhost:5432/postgres';
 
@@ -8,16 +7,14 @@ module.exports = {
     deleteFile: function (req, res) {
         var id = req.params.id;
         pg.connect(connectionString, function (err, client, done) {
-            var query = client.query("DELETE FROM content WHERE file_id = ($1)", [id]);
-            var query2 = client.query("DELETE FROM file WHERE id = ($1)", [id]);
-
-            query2.on('end', function () {
-                client.end();
-                return res.json("");
+            var deleteContent = client.query("DELETE FROM content WHERE file_id = ($1)", [id]);
+            deleteContent.on('end', function () {
+                var deleteFile = client.query("DELETE FROM file WHERE id = ($1)", [id]);
+                deleteFile.on('end', function () {
+                    client.end();
+                    return res.json("");
+                });
             });
-            if (err) {
-                console.log(err);
-            }
         });
     },
     addFiles: function (req, res) {
@@ -37,19 +34,24 @@ module.exports = {
                 //res.writeHead(200, {'content-type': 'text/plain'});
                 //res.end('received files:\n\n ' + util.inspect(files));
             });
-        form.parse(req);
-        return res.json("");
-        /*pg.connect(connectionString, function (err, client, done) {
-         var query = client.query("INSERT INTO FILE ( name, ratio ) values ($1, $2)", [files[0].name], 1);
-         //var query2 = client.query("INSERT INTO CONTENT(file_id, type, content) VALUES ($1, $2, $3)", []);
-
-         query.on('end', function () {
-         client.end();
-         return res.json("");
-         });
-         if (err) {
-         console.log(err);
-         }
-         });*/
     }
 };
+
+/*pg.connect(connectionString, function (err, client, done) {
+ for (var i = 0; i < files.length; i++) {
+ console.log("any post?")
+ var file = files[i];
+ var name = file.name;
+ var insertFile = client.query("INSERT INTO FILE ( name, ratio ) values ($1, $2)", [name], 1);
+ insertFile.on('end', function () {
+ console.log("123");
+ /!*var insertContent = client.query("INSERT INTO CONTENT(file_id, type, content) VALUES ($1, $2, $3)", [10, 'picture', file._writeStream]);
+ insertContent.on('end', function () {*!/
+ client.end();
+ return res.json("");
+ //});
+ });
+
+ }
+ })
+ ;*/
